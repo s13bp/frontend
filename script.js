ac94e20f0f92b67eb051d502f0560b5a2410cb49
s13bp <86206902+s13bp@users.noreply.github.com>
2023-05-02 02:01:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // fetch data from api
-    const response = await fetch('https://back-ai.vercel.app', {
+    const response = await fetch('https://ia-c9z8.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
